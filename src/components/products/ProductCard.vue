ea12d7d4314fd4843c1affe3f15f353a5f375e78
marioshafiek <marioshafiek12@gmail.com>
2024-09-16 16:57:46 +0300

    refactor: Edit Card

--- src/components/products/ProductCard.vue
+++ src/components/products/ProductCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border-2 border-black w-[400px] h-[500px] flex flex-col justify-between">
+  <div class="border-2 border-black w-[300px] h-[350px] flex flex-col justify-between">
     <div>
       <div class="relative">
         <div
@@ -8,15 +8,25 @@
           {{ product.price }}
         </div>
       </div>
-      <!-- Used here [400px] instaed of h-100 -->
-      <img :src="product.image" alt="Item" class="w-full h-[400px]" />
+      <img
+        :src="product.image"
+        alt="Item"
+        class="w-[200px] h-[200px] ml-10 mt-7 object-contain flex justify-center items-center"
+      />
     </div>
     <div class="p-4 flex justify-between">
-      <p class="text-lg font-semibold mb-2">{{ product.title }}</p>
-      <button class="border-2 border-black w-20">Add +</button>
+      <p class="text-lg font-semibold mb-2 h-20 w-40">{{ product.title.slice(0, 20) }}...</p>
+      <button
+        @click="animateButton"
+        :class="{ 'animate-scale': isAnimating }"
+        class="border-2 border-black w-20 h-8"
+      >
+        Add +
+      </button>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   props: {
@@ -24,8 +34,40 @@ export default {
       type: Object,
       required: true
     }
+  },
+  data() {
+    return {
+      isAnimating: false
+    }
+  },
+  methods: {
+    animateButton() {
+      this.isAnimating = !this.isAnimating
+      setTimeout(() => {
+        this.isAnimating = false
+      }, 300)
+    }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+@keyframes scale {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  80% {
+    transform: scale(0.8);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+.animate-scale {
+  animation: scale 0.3s ease-in-out;
+}
+</style>
--- src/components/products/ProductCardList.vue
+++ src/components/products/ProductCardList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4">
       <ProductCard v-for="(product, index) in products" :key="index" :product="product" />
     </div>
   </div>
@@ -13,7 +13,6 @@ export default {
   components: {
     ProductCard
   },
-  //Data
   data() {
     return {
       products: []
