2b2d992c1b579b0f377b2688fa54c9bdf8436396
marioshafiek <58153069+marioshafiek@users.noreply.github.com>
2024-09-16 16:08:44 +0300

    Product Card Design (#2)
    * feat: Product Card Design
    
    * refactor: Edit Product Card and Adding Home Page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <NavBar />
+  <Home />
 </template>
 <script>
 import NavBar from './components/shared/NavBar.vue'
+import Home from './pages/Home.vue'
 export default {
   components: {
-    NavBar
+    NavBar,
+    Home
   }
 }
 </script>
---
+++ src/components/products/ProductCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="border-2 border-black w-[400px] h-[500px] flex flex-col justify-between">
+    <div>
+      <div class="relative">
+        <div
+          class="w-20 h-10 bg-black text-white absolute right-2 top-2 flex items-center justify-center"
+        >
+          {{ product.price }}
+        </div>
+      </div>
+      <!-- Used here [400px] instaed of h-100 -->
+      <img :src="product.image" alt="Item" class="w-full h-[400px]" />
+    </div>
+    <div class="p-4 flex justify-between">
+      <p class="text-lg font-semibold mb-2">{{ product.title }}</p>
+      <button class="border-2 border-black w-20">Add +</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
