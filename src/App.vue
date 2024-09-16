45b8b508b3e64de8892f324245ce6c1d86125fc5
marioshafiek <marioshafiek12@gmail.com>
2024-09-16 13:33:46 +0300

    chore: Delete unused code

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,9 @@
 </template>
 <script>
 import NavBar from './components/shared/NavBar.vue'
-import ProductCardListing from './components/products/ProductCardListing.vue'
 export default {
   components: {
-    NavBar,
-    ProductCardListing
+    NavBar
   }
 }
 </script>
