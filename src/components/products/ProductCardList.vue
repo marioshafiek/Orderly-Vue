422e9d9dd50211cecf4b4d364da9e53b54b501e4
marioshafiek <marioshafiek12@gmail.com>
2024-09-17 11:37:43 +0300

    fix: fix mobile responsiveness

--- src/components/products/ProductCardList.vue
+++ src/components/products/ProductCardList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4">
+  <div class="p-5">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-10">
       <ProductCard v-for="(product, index) in allProducts" :key="index" :product="product" />
     </div>
   </div>
