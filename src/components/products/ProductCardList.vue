29282d9e6f8bf032ab614cb662ede871e54c2af1
marioshafiek <marioshafiek12@gmail.com>
2024-09-17 12:19:54 +0300

    fix: mobile responsiveness with display flex

--- src/components/products/ProductCardList.vue
+++ src/components/products/ProductCardList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-5">
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 px-10">
+  <div class="flex justify-center">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4">
       <ProductCard v-for="(product, index) in allProducts" :key="index" :product="product" />
     </div>
   </div>
