e6ae32bd925ee06a0a3e1dcdcedc55bf6bd9d6d8
marioshafiek <marioshafiek12@gmail.com>
2024-09-17 13:32:42 +0300

    feat: Cart Drawer with cart items

--- src/components/Ecom/Cart.vue
+++ src/components/Ecom/Cart.vue
@@ -5,14 +5,26 @@
       <img class="w-9" src="../../assets/Cart.svg" alt="CartItem" />
     </div>
   </button>
+  <CartDrawer :visible="isCartVisible" @update:visible="isCartVisible = $event" />
 </template>
+
 <script>
+import CartDrawer from './CartDrawer.vue'
 export default {
+  components: {
+    CartDrawer
+  },
+  data() {
+    return {
+      isCartVisible: false
+    }
+  },
   methods: {
     handleClick() {
-      console.log('Clicked')
+      this.isCartVisible = !this.isCartVisible
     }
   }
 }
 </script>
-<style></style>
+
+<style scoped></style>
---
+++ src/components/Ecom/CartItemList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col gap-10">
+    <CartItem />
+    <CartItem />
+  </div>
+</template>
+
+<script>
+import CartItem from './CartItem.vue'
+export default {
+  components: {
+    CartItem
+  }
+}
+</script>
+
+<style></style>
